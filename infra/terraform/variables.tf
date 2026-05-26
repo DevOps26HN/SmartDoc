57ece2c1be5eff542149c80106f5456797e4e811
hasan-bekir-dogan <hasanbekirdogan@mac.speedport.ip>
2026-05-26 22:53:20 +0200

    feat(terraform): provision Azure VM infrastructure - closes #61

---
+++ infra/terraform/variables.tf
@@ -0,0 +1,23 @@
+variable "resource_group_name" {
+  default = "smartdoc-rg"
+}
+
+variable "location" {
+  default = "West Europe"
+}
+
+variable "vm_name" {
+  default = "smartdoc-vm"
+}
+
+variable "admin_username" {
+  default = "azureuser"
+}
+
+variable "vm_size" {
+  default = "Standard_B2s_v2"
+}
+
+variable "ssh_public_key_path" {
+  default = "~/.ssh/id_rsa.pub"
+}
